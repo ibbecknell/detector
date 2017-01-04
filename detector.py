aff50d5e8bdf677f5bbdf96ccbb277c7607d29a2
India Buckley <ibbecknell@gmail.com>
2017-01-04 00:10:10 +0000

    added image capture at specified time: goes to images folder

--- detector.py
+++ detector.py
@@ -1,23 +1,41 @@
 import RPi.GPIO as GPIO
-
 import time
+from picamera import PiCamera
+from datetime import datetime
+
+sensor = 4
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
 
-PIR_PIN = 7
-GPIO.setup(PIR_PIN, GPIO.IN)
+previous_state = False
+current_state = False
+camera = PiCamera()
 
-try:
-    print "PIR Module Test"
-    print "Preparing the sensor"
-    time.sleep(2)
-    print "Ready!"
+while True:
+    time.sleep(0.1)
+    previous_state = current_state
+    current_state = GPIO.input(sensor)
+    i = 0
+    if current_state:
+        i = time.clock()
+        print("motion detected %s" % (i)) 
+        if i > 0.05:
+            print("time limit met")
+            filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.jpg")
+            camera.capture("/home/pi/detector/images/%s"%filename)
+            #            camera.capture("%s.jpg" % filename)
+        else:
+            continue
+            #   camera.start_recording(filename)
+    #if !current_state:
+        #camera.stop_recording()
 
-    while True:
-        if GPIO.input(PIR_PIN):
-            print "Motion Detected!"
-
-        time.sleep(1)
-except KeyboardInterrupt:
-    print "Quit!"
-    GPIO.cleanup()
+    # if current_state != previous_state:
+    #     new_state = "HIGH" if current_state else "LOW"
+    #     print("GPIO pin %s is %s" % (sensor, new_state))
+    #     timing = 0.0
+    #     if new_state == "HIGH":
+    #         timing = time.clock()
+    #         if timing >= 5:
+    #             print("time limit reached")
